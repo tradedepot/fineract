ce4bb0e532d7ddbf8d5cb0e569f3e012d8fce595
Uchenna <uokafor@c2gconsulting.com>
2020-08-27 14:35:13 +0100

    circle ci

---
+++ deploy.sh
@@ -0,0 +1,20 @@
+IFS=$'\t'
+array=("develop\tmaster")
+unset IFS
+
+value="$CIRCLE_BRANCH"
+echo ${value}
+
+# Colors for cosmetics
+RED='\033[0;31m'
+YELLOW='\033[0;33m'
+GREEN='\033[0;32m'
+
+if [[ "\t${array[@]}\t" =~ "\t${value}\t" ]]; then
+    echo -e "${YELLOW}Starting deployment of $CIRCLE_BRANCH to docker"
+    docker login -e $DOCKER_EMAIL -u $DOCKER_USERNAME -p $DOCKER_PASS
+    ./scripts/dockerbuild/tagandpush.sh
+    echo -e "${GREEN} Done deploying of $CIRCLE_BRANCH to docker"
+fi
+
+exit 0;
