5090caff96815319137e851953f0544fe737f142
Uchenna <uokafor@c2gconsulting.com>
2020-08-27 15:38:26 +0100

    add tag and push to docker

--- deploy.sh
+++ deploy.sh
@@ -13,7 +13,7 @@ GREEN='\033[0;32m'
 if [[ "\t${array[@]}\t" =~ "\t${value}\t" ]]; then
     echo -e "${YELLOW}Starting deployment of $CIRCLE_BRANCH to docker"
     docker login -e $DOCKER_EMAIL -u $DOCKER_USERNAME -p $DOCKER_PASS
-    ./scripts/dockerbuild/tagandpush.sh
+    ./tagandpush.sh
     echo -e "${GREEN} Done deploying of $CIRCLE_BRANCH to docker"
 fi
 
